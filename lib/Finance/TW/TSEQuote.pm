99c34cc39f83e1ae1355922ffc120474374cf054
Chia-liang Kao (高嘉良) <clkao@clkao.org>
2012-04-24 14:57:56 +0000

    Import of CLKAO/Finance-TW-TSEQuote-0.28 from CPAN.
    gitpan-cpan-distribution: Finance-TW-TSEQuote
    gitpan-cpan-version:      0.28
    gitpan-cpan-path:         CLKAO/Finance-TW-TSEQuote-0.28.tar.gz
    gitpan-cpan-author:       CLKAO
    gitpan-cpan-maturity:     released

--- lib/Finance/TW/TSEQuote.pm
+++ lib/Finance/TW/TSEQuote.pm
@@ -1,30 +1,47 @@
 package Finance::TW::TSEQuote;
-$VERSION = '0.27';
-
+use 5.10.1;
 use strict;
+our $VERSION = '0.28';
+
 use LWP::Simple ();
-eval { require 'Encode::compat' };
 use Encode 'from_to';
 use URI::Escape;
+use App::Cache;
+use Digest::MD5 qw(md5_hex);
 
 sub resolve {
-    my $self = shift if ref($_[0]) eq __PACKAGE__;
-    shift if $_[0] eq __PACKAGE__;
+    my $self = shift;
+    $self = bless {}, __PACKAGE__ unless ref($self) eq __PACKAGE__;
     my $name = shift;
+    $self->{cache} ||= App::Cache->new({ ttl => 7 * 24 * 60 * 60 });  # a week
+    my $cache = $self->{cache};
+    my $key   = md5_hex($name);
 
-    from_to($name, 'utf-8', 'big5');
+    unless ($cache->get($key)) {
+        my $url = "http://brk.twse.com.tw:8000/isin/C_public.jsp?strMode=2";
+        my $content = $cache->get_url($url);
 
-    $name = uri_escape($name);
-
-    my $content = LWP::Simple::get("http://mops.tse.com.tw/server-java/t05st49_1?step=1&kinds=sii&colorchg=1&type=01&nick_name=$name");
-
-    my ($id, $fullname, $engname) = $content =~ m|<td>(\d+)&nbsp;</td><td>(.*?)&nbsp;</td><td>(.*?)&nbsp;</td></tr>|;
+        from_to($content, 'big5', 'utf-8');
+        use HTML::TableExtract;
+        my $te = HTML::TableExtract->new(
+                            headers => [qw(證券代號及名稱 上市日)]);
+        $te->parse($content);
+        foreach my $ts ($te->tables) {
+            foreach my $row ($ts->rows) {
+                my ($symbol, $company)
+                    = $row->[0] =~ m|(\S+)\s+\xe3\x80\x80(.*?)$|o;
+                next unless $symbol;
+                my $board_date = $row->[1];
+                $cache->set(md5_hex($company),
+                            { id => $symbol, date => $board_date });
+            }
+        }
+    }
+    my $id = $cache->get($key)->{id};
 
     die "can't resolve symbol: $name" unless $id;
 
-    from_to($fullname, 'big5', 'utf-8');
-
-    @{$self}{qw/id fullname engname/} = ($id, $fullname, $engname);
+    @{$self}{qw/id/} = ($id);
 
     return $id;
 
@@ -35,7 +52,7 @@ sub new {
     my $self = bless {}, $class;
 
     $self->resolve($target)
-	unless $target =~ /^\d+$/;
+        unless $target =~ /^\d+$/;
 
     $self->{id} ||= $target;
 
@@ -48,35 +65,36 @@ no encoding;
 sub get {
     my $self = shift;
     my $stockno = ref $self ? $self->{id} : shift;
-    my $content = LWP::Simple::get("http://mis.tse.com.tw/data/$stockno.csv");
+    my $content
+        = LWP::Simple::get("http://mis.twse.com.tw/data/$stockno.csv");
     from_to($content, 'big5', 'utf-8');
 
     my $result;
     $content =~ s/["\n\r]//g;
     my @info = split /,/, $content;
-    my $cmap = [undef, 'UpDown', 'time', 'UpPrice', 'DownPrice', 'OpenPrice',
-		'HighPrice', 'LowPrice', 'MatchPrice', 'MatchQty', 'DQty'];
-    $result->{$cmap->[$_]} = $info[$_] foreach (0..10);
+    my $cmap = [ undef,        'UpDown',    'time',      'UpPrice',
+                 'DownPrice',  'OpenPrice', 'HighPrice', 'LowPrice',
+                 'MatchPrice', 'MatchQty',  'DQty' ];
+    $result->{ $cmap->[$_] } = $info[$_] foreach (0 .. 10);
     $result->{name} = $info[32];
     $result->{name} =~ s/\s//g;
     $self->{name} ||= $result->{name} if ref $self;
 
     if ($result->{MatchPrice} == $result->{UpPrice}) {
-	$result->{UpDownMark} = '♁';
-    }
-    elsif ($result->{MatchPrice} == $result->{DownPrice}) {
-	$result->{UpDownMark} = '?';
-    }
-    elsif ($result->{UpDown} > 0) {
-	$result->{UpDownMark} = '＋';
-    }
-    elsif ($result->{UpDown} < 0) {
-	$result->{UpDownMark} = '－';
+        $result->{UpDownMark} = '♁';
+    } elsif ($result->{MatchPrice} == $result->{DownPrice}) {
+        $result->{UpDownMark} = '?';
+    } elsif ($result->{UpDown} > 0) {
+        $result->{UpDownMark} = '＋';
+    } elsif ($result->{UpDown} < 0) {
+        $result->{UpDownMark} = '－';
     }
 
-    $result->{Bid}{Buy}[$_]{$info[11+$_*2]} = $info[12+$_*2] foreach (0..4);
-    $result->{Bid}{Sell}[$_]{$info[21+$_*2]} = $info[22+$_*2] foreach (0..4);
-    $result->{BuyPrice} = $info[11];
+    $result->{Bid}{Buy}[$_]{ $info[ 11 + $_ * 2 ] } = $info[ 12 + $_ * 2 ]
+        foreach (0 .. 4);
+    $result->{Bid}{Sell}[$_]{ $info[ 21 + $_ * 2 ] } = $info[ 22 + $_ * 2 ]
+        foreach (0 .. 4);
+    $result->{BuyPrice}  = $info[11];
     $result->{SellPrice} = $info[21];
 
     $self->{quote} = $result if ref $self;
@@ -84,6 +102,57 @@ sub get {
     return $result;
 }
 
+sub fetchMarketFile {
+    my $self = shift;
+    my ($stock, $year, $month) = @_;
+    my @fields = ();
+    my ($i, $url, $file, $arg, $outfile);
+
+    $month = "0" . $month if $month < 10;
+    $url
+        = "http://www.twse.com.tw/ch/trading/exchange/STOCK_DAY/genpage/Report"
+        . $year
+        . $month . "/";
+    $file = $year . $month . "_F3_1_8_" . $stock . ".php?STK_NO=" . $stock;
+    $arg  = "&myear=" . $year . "&mmon=" . $month;
+    my $content = LWP::Simple::get("$url$file$arg");
+    my $result;
+
+    if ($content) {
+        if ($content =~ /<tr bgcolor='#F7F0E8'>(.+)/) {
+            $content = $1;
+            $content =~ s/<table(.)*?>/ /g;
+            $content =~ s/<tr(.)*?>/ /g;
+            $content =~ s/<td(.)*?>/ /g;
+            $content =~ s/<\/tr(.)*?>/ /g;
+            $content =~ s/<\/td(.)*?>/ /g;
+            $content =~ s/<div(.)*?>/ /g;
+            $content =~ s/<\/div(.)*?>/ /g;
+            $content =~ s/&nbsp;/ /g;
+            $content =~ s/.*µ§¼Æ\s*//;
+            $content =~ s/\s+/ /g;
+            $content =~ s/,//g;
+            @fields = split / /, $content;
+
+            for ($i = 18; $i <= $#fields; $i += 9) {
+                my $date = $fields[ $i - 3 ];
+                my ($yy, $mm, $dd) = split /\//, $date;
+                $fields[ $i - 3 ] = (1911 + $yy) . "-" . $mm . "-" . $dd
+                    if $mm;
+
+                $result
+                    .= $fields[$i] . "\t"
+                    . $fields[ $i + 1 ] . "\t"
+                    . $fields[ $i + 2 ] . "\t"
+                    . $fields[ $i + 3 ] . "\t"
+                    . $fields[ $i + 5 ] . "\t"
+                    . $fields[ $i - 3 ] . "\n";
+
+            }
+        }
+    }
+    return $result;
+}
 
 1;
 
@@ -117,6 +186,11 @@ symbol, as well as getting the real time quote.
 =item resolve
 
     Resolve the company name to stock symbol.
+
+=item fetchMarketFile
+
+    Fetch the Een-Of-Day stock information for specific company
+	by year and month.
 
 =item get
 
@@ -161,9 +235,11 @@ symbol, as well as getting the real time quote.
 
 Chia-liang Kao E<lt>clkao@clkao.orgE<gt>
 
+Cheng-Lung Sung
+
 =head1 COPYRIGHT
 
-Copyright 2003 by Chia-liang Kao E<lt>clkao@clkao.orgE<gt>.
+Copyright 2003-2012 by Chia-liang Kao E<lt>clkao@clkao.orgE<gt>.
 
 This program is free software; you can redistribute it and/or modify it
 under the same terms as Perl itself.
